8e7992fe958478cc04b4ac10fb6d9d67afd70f19
Ohashi1409 <ohashiramos@gmail.com>
2024-03-15 12:45:17 -0300

    Atualizacao codigo inimigo

--- Projeto/scripts/enemy.py
+++ Projeto/scripts/enemy.py
@@ -1,8 +1,10 @@
+#importando pygame e livraria necessaria
 import pygame
 from pygame.locals import *
 import time
 
 pygame.init()
+
 # Criar classe de inimigo
 enemy_img = pygame.image.load('Projeto/assets/coruja-Sheet.png')
 
@@ -16,6 +18,7 @@ def load_crop_image(img, x, y, w, h, transform=True):
 
 class base_enemy(pygame.sprite.Sprite):
     def __init__(self, x, y, x_change):
+        #renderizando sprite
         pygame.sprite.Sprite.__init__(self)
         self.speed = 10
         self.life = 2
@@ -26,7 +29,7 @@ class base_enemy(pygame.sprite.Sprite):
         self.images_right = []
         self.image_atual = []
 
-        for c in range(0, 448, 64):
+        for c in range(0, 320, 64):
             left = load_crop_image(enemy_img, c, 96, 64, 64, False)
             left = pygame.transform.scale(left, (48,96))
             self.images_left.append(left)
@@ -35,9 +38,10 @@ class base_enemy(pygame.sprite.Sprite):
             up = pygame.transform.scale(up, (48,96))
             self.images_up.append(up)
             
-            right = load_crop_image(enemy_img, c, 32, 64, 64, False)
-            right = pygame.transform.scale(right, (48,96))
-            self.images_right.append(right)
+            if c <= 128:
+                right = load_crop_image(enemy_img, c, 64, 64, 64, False)
+                right = pygame.transform.scale(right, (96,128))
+                self.images_right.append(right)
             
             down = load_crop_image(enemy_img, c, 0, 64, 64, False)
             down = pygame.transform.scale(down, (96,128))
@@ -49,17 +53,19 @@ class base_enemy(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
         self.pos_y_inicial = 100 - 64 - 96//2
-        self.rect.topleft = (500, 300)
+        self.rect.topleft = (1280, 300)
     
     def update(self):
         self.i += 0.20
         if self.i >= len(self.images_down):
             self.i = 0
         self.image = self.image_atual[int(self.i)]
+
+        self.movement()
     
     # Definir movimento
     def movement(self):
-        x = self.x
+        x = self.rect.x
         x_change = self.x_change
 
         if x <= 100:
@@ -74,6 +80,7 @@ class base_enemy(pygame.sprite.Sprite):
             time.sleep(1.5)
         elif x == 100 :
             time.sleep(1.5)
+            self.image_atual = self.images_right
         elif self.colisao():
             time.sleep(1)
 
@@ -81,25 +88,25 @@ class base_enemy(pygame.sprite.Sprite):
 
         self.colisao()
 
-        self.x = x
+        self.rect.x = x
         self.x_change = x_change
 
+    #difinindo colisao
     def colisao(self):
         collided = False
 
-        if self.x == 100:
+        if self.rect.x == 100:
             collided = True
 
         if collided:
             self.life -= 1
 
         return collided
-
+        
 # Inicializar valores
 width_screen = 1280
 width = 1050
 height = 200
 pause = [1,2,3,4,5,6,7,8,9,10,11,12,13]
 cont = []
-# Renderizar sprite
 # Definir máscara de colisão
